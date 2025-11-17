03fec30ab80a1c88a50d568eb3fbb04fcb911def
koocyy <krystof.oczadly@gmail.com>
2025-11-17 19:33:59 +0100

    made sendmessage error resistant

--- messages.py
+++ messages.py
@@ -12,29 +12,32 @@ MASTER_KEY = '$2a$10$CmT1z5R8IU3f.vQP.uitxuGo8J0nTGTGKBwZIEU89yqki62s7pwfS'
 def sendmessage(nickname, message):
     global MASTER_KEY
 
-    with open(f"{key_dir}/{nickname}_public_key.pem", "rb") as f:
-        public_key = rsa.PublicKey.load_pkcs1(f.read())
-    enc_message = rsa.encrypt(message.encode(), public_key)
+    try:
+        with open(f"{key_dir}/{nickname}_public_key.pem", "rb") as f:
+            public_key = rsa.PublicKey.load_pkcs1(f.read())
+        enc_message = rsa.encrypt(message.encode(), public_key)
 
-    headry = {
-        'X-Master-Key': MASTER_KEY
-    }
-    PUTheadry = {
-        'Content-Type': 'application/json',
-        'X-Master-Key': MASTER_KEY
-    }
-    json_url = "https://api.jsonbin.io/v3/b/691a506843b1c97be9b1c553"
-    req = requests.get(url=json_url, json=None, headers=headry)
-    mess = req.json()
-    messages = mess['record']
-    print(str(messages) + "\n-----------------\n")
-    n_object = nickname
-    n_val = str(enc_message)
-    messages[n_object] = n_val
-    new_mess = json.dumps(messages, indent=2)
-    print(new_mess)
-    update = requests.put(url=json_url, data=new_mess, headers=PUTheadry)
-    return print(update.status_code)
+        headry = {
+            'X-Master-Key': MASTER_KEY
+        }
+        PUTheadry = {
+            'Content-Type': 'application/json',
+            'X-Master-Key': MASTER_KEY
+        }
+        json_url = "https://api.jsonbin.io/v3/b/691a506843b1c97be9b1c553"
+        req = requests.get(url=json_url, json=None, headers=headry)
+        mess = req.json()
+        messages = mess['record']
+        print(str(messages) + "\n-----------------\n")
+        n_object = nickname
+        n_val = str(enc_message)
+        messages[n_object] = n_val
+        new_mess = json.dumps(messages, indent=2)
+        print(new_mess)
+        update = requests.put(url=json_url, data=new_mess, headers=PUTheadry)
+        return print(update.status_code)
+    except:
+        print(Fore.LIGHTRED_EX + "You dont have that user added or used a space at the end." + Style.RESET_ALL)
 
 
 
